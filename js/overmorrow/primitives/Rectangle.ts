a2f339371283251b98e853bccead57aa157c1f94
Ben Sergent V <bsergent21897@gmail.com>
2017-11-10 18:08:00 -0500

    Convert Rectangle interface to class and add intersection method

--- js/overmorrow/primitives/Rectangle.ts
+++ js/overmorrow/primitives/Rectangle.ts
@@ -1,6 +1,23 @@
-export default interface Rectangle {
-  x:number,
-  y:number,
-  width:number,
-  height:number
+export default class Rectangle {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = x + width;
+    this.y2 = y + height;
+  }
+
+  get width(): number {
+    return this.x2 - this.x1;
+  }
+  get height(): number {
+    return this.y2 - this.y1;
+  }
+  intersects(rect: Rectangle): boolean {
+    return this.x1 < rect.x2 && this.x2 > rect.x1 && this.y1 < rect.y2 && this.y2 > rect.y1;
+  }
 }
